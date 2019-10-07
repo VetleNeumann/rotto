1ee07bc2279b97949b8eb5489a526b41f5719030
Zantox <vetle_er_her@hotmail.com>
2019-10-07 15:41:21 +0200

    Added basic movement and baton swinging

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    GameObject rotto;
+    public float yOffset = 9f;
+    public float zOffset = 0f;
+    public float xRotation = 80f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotto = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnValidate()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(xRotation, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = rotto.transform.position + new Vector3(0, yOffset, zOffset);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,24 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerManager))]
 public class PlayerController : MonoBehaviour
 {
     PlayerManager playerManager;
+    CameraManager cameraManager;
+
+    float accelrate = 52f;
 
     // Start is called before the first frame update
     void Start()
     {
         playerManager = GetComponent<PlayerManager>();
+        cameraManager = Camera.main.GetComponent<CameraManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         BatonControl();
+        MovementControl();
     }
 
     void BatonControl()
     {
-        
+        Vector2 mousePosition = Input.mousePosition;
+        //Calculates the relative position of the mouse cursor, which can be anywhere
+        //between (-1,-1) (bottom left), or (1,1) (upper right).
+        Vector2 relativeMousePosition = 2f * new Vector2(mousePosition.x / Screen.width, mousePosition.y / Screen.height) - Vector2.one;
+        //Limits the maximum and minimum value of the mouse position in case the cursor
+        //is outside the screen.
+        relativeMousePosition = new Vector2(Mathf.Clamp(relativeMousePosition.x, -1f, 1f), Mathf.Clamp(relativeMousePosition.y, -1f, 1f));
+
+        //Calculates the angle about the center.
+        Quaternion mouseCenterAngle = Quaternion.Euler(0f, Vector2.SignedAngle(Vector2.down, relativeMousePosition), 0f);
+        mouseCenterAngle.x *= -1f;
+        mouseCenterAngle.w *= -1f;
+
+        playerManager.BatonRotate(mouseCenterAngle);
+    }
+
+    void MovementControl()
+    {
+        Vector2 inputs = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        playerManager.Move(inputs * accelrate);
     }
 }
