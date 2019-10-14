9d97acb331b8d965a950a9edd3e5c8e0ec416cff
Zantox <vetle_er_her@hotmail.com>
2019-10-14 19:49:49 +0200

    Added basic door opening and closing functionality
    The buttons aren't made yet so they won't open or close but the functionality for doing it is inside the DoorController script.

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DoorManager))]
+public class DoorController : MonoBehaviour
+{
+    DoorManager doorManager;
+
+    bool open = false;
+    bool moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorManager = GetComponent<DoorManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+            moving = !doorManager.MoveDoor(open);
+    }
+
+    void ToggleDoor()
+    {
+        open = !open;
+        moving = true;
+    }
+}
---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    Transform leftDoor;
+    Transform rightDoor;
+
+    //How much of the door should remain in sight
+    float doorRemain = 0.25f;
+    float closedTolerance = 0.05f;
+    float doorWidth = 1f;
+    float doorSpeed = 0.08f;
+
+    Vector3 initialPosLeft;
+    Vector3 initialPosRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            switch (child.name)
+            {
+                case "Door Left":
+                    leftDoor = child;
+                    initialPosLeft = leftDoor.position;
+                    break;
+                case "Door Right":
+                    rightDoor = child;
+                    initialPosRight = rightDoor.position;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool MoveDoor(bool open)
+    {
+        leftDoor.position = Vector3.Lerp(leftDoor.position, initialPosLeft + Vector3.left * (open ? 1f - doorRemain : 0f), doorSpeed);
+        rightDoor.position = Vector3.Lerp(rightDoor.position, initialPosRight + Vector3.right * (open ? 1f - doorRemain : 0f), doorSpeed);
+        
+        if (open && (leftDoor.position - (initialPosLeft + Vector3.left * (1 - doorRemain))).magnitude < closedTolerance)
+        {
+            leftDoor.position = initialPosLeft + Vector3.left * (1 - doorRemain);
+            rightDoor.position = initialPosRight + Vector3.right * (1 - doorRemain);
+            return true;
+        }
+        else if (!open && (leftDoor.position - initialPosLeft).magnitude < closedTolerance)
+        {
+            leftDoor.position = initialPosLeft;
+            rightDoor.position = initialPosRight;
+            return true;
+        }
+        return false;
+    }
+}
