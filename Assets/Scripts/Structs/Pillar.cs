b063ef842fba686b6f7fa8d66c1d789347a61e8a
Zantox <vetle_er_her@hotmail.com>
2019-10-16 05:05:00 +0200

    Added Pillar struct and ColorRGB enum
    Pillar struct and ColorRGB enum added to be used for pillar puzzles, in addition to adding folders intended to housing enums and structs.

---
+++ Assets/Scripts/Structs/Pillar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Pillar
+{
+    public Pillar(Transform transform, ColorRGB color)
+    {
+        this.transform = transform;
+        this.color = color;
+        this.activated = false;
+    }
+
+    public Transform transform;
+    public ColorRGB color;
+    public bool activated;
+}
