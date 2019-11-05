46f6820345ffac66544c4e85061145b5682abfa2
Evengit config --global user.name "Even"git config --global user.name "Even <eventr3@gmail.com>
2019-11-06 00:30:22 +0100

    Worked on Health System

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Linq;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    HealthManager healthManager = new HealthManager();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //healthManager.SetHealth(100);
+        
+    }
+
+    // Update is called once per frame
+    
+     void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "weapon")
+        {
+            healthManager.AddHealth(1);
+        }
+    }
+
+    
+}
