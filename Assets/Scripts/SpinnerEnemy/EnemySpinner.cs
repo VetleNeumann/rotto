e79ea9e3728a1224c5ba2e14a1eb23e53dd1d668
Evengit config --global user.name "Even"git config --global user.name "Even <eventr3@gmail.com>
2019-11-12 23:41:58 +0100

    Worked on spinner enemy

---
+++ Assets/Scripts/SpinnerEnemy/EnemySpinner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpinner : MonoBehaviour
+{
+    public Transform target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(new Vector3(0f, 90f, 0f), 0.1f, Space.Self);
+       
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Baton")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
