75a069f03708481230245fd6046142b25b5ba018
Zantox <vetle_er_her@hotmail.com>
2019-11-17 13:50:23 +0100

    Added the turnstile puzzle again

--- Assets/Scripts/TurnstileController.cs
+++ Assets/Scripts/TurnstileController.cs
@@ -4,23 +4,74 @@ using UnityEngine;
 
 public class TurnstileController : ControllerBase
 {
-    Rigidbody2D rigidBody;
-    BoxCollider2D batonCollider;
+    bool firstPress = false;
+
+    Rigidbody rigidBody;
+    BoxCollider batonCollider;
+
+    [SerializeField]
+    Material red, green;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponentInChildren<Rigidbody2D>();
-        FreezeRotation(true);
-    }
 
-    void FreezeRotation(bool freeze)
-    {
-        rigidBody.freezeRotation = freeze;
     }
 
     public override void ButtonPressed()
     {
-        FreezeRotation(false);
+        //FreezeRotation(false);
+        if (!firstPress)
+
+        StartCoroutine(ButtonToggled());
+        firstPress = true;
+    }
+
+    IEnumerator ButtonToggled()
+    {
+        Transform turnstile = GameObject.Find("Turnstile").transform;
+        MeshRenderer button = new MeshRenderer();
+        Light light = new Light();
+        foreach (Transform child in transform)
+        {
+            switch (child.tag)
+            {
+                case "Button":
+                    button = child.GetComponent<MeshRenderer>();
+                    print("found");
+                    break;
+                case "Light":
+                    light = child.GetComponent<Light>();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (button == new MeshRenderer())
+        {
+            Debug.LogError("Couldn't find button.");
+            yield break;
+        }
+        else if (light == new Light())
+        {
+            Debug.LogError("Couldn't find light.");
+            yield break;
+        }
+
+        yield return new WaitForSeconds(0.3f);
+        //Shut off light
+        light.enabled = false;
+        yield return new WaitForSeconds(0.8f);
+        //Turn light green
+        light.color = Color.green;
+        button.material = green;
+        yield return new WaitForSeconds(0.35f);
+        //Turn light on
+        light.enabled = true;
+        //Play sound
+
+        //Unfreeze turnstile
+        turnstile.GetComponent<Rigidbody>().constraints &= ~RigidbodyConstraints.FreezeRotationY;
     }
 }
