3a5c9841918a3b1b5d6f2bc6765cbeb8213bd57b
Zantox <vetle_er_her@hotmail.com>
2019-11-26 20:46:10 +0100

    Created a simple mob spawner prefab
    No respawn logic yet.

---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnEnemy(GameObject prefab)
+    {
+        //Spawn enemy
+        Instantiate(prefab, transform.position + Vector3.up, Quaternion.identity, transform.parent);
+        //Maybe add animation of enemy coming up from ground
+    }
+}
