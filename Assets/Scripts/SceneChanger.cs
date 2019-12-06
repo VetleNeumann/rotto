dc4670f8bd180b3581c4c263367ad6227e02bb1f
Zantox <vetle_er_her@hotmail.com>
2019-12-06 21:44:01 +0100

    Added scene fading and changing

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    Animator animator;
+
+    int levelToLoad;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
---
+++ Assets/Scripts/Utility/SceneChangeTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChangeTrigger : MonoBehaviour
+{
+    [SerializeField]
+    int sceneIndex;
+
+    void OnTriggerEnter()
+    {
+        GameObject.Find("SceneChanger").GetComponent<SceneChanger>().FadeToLevel(sceneIndex);
+    }
+}
