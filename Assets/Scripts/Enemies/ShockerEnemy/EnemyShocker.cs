1947f6b0768a85b52bafc5d505d73854d2a6d333
Evengit config --global user.name "Even"git config --global user.name "Even <eventr3@gmail.com>
2019-12-07 01:09:51 +0100

    Shocker Enemy mostly done

--- Assets/Scripts/Enemies/ShockerEnemy/EnemyShocker.cs
+++ Assets/Scripts/Enemies/ShockerEnemy/EnemyShocker.cs
@@ -1,49 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 using System.Timers;
 
-public class EnemyShocker : MonoBehaviour
+public class EnemyShocker : BaseEnemy
 {
-    public MeshRenderer[] MaterialZone;
+    MeshRenderer[] MaterialZone;
     private static System.Timers.Timer clock;
     bool ShockOn = false;
-    public float timer = 2f;
+    public float timer = 4f;
     public float state = 0f;
+    Renderer[] rend;
+    Transform[] tagg;
+    Transform[] scale;
+    public Transform target;
+    float size = 0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-       
-
-
+        SetRender(false);
+        SetGametag("Untagged");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        //agent.destination = target.position;
         timer -= Time.deltaTime;
-        if(timer <= 10f && state == 0f)
+        if (timer <= 3f && state == 0f)
         {
-            SetMaterial(Color.green);
-            state = 1f; 
+            SetRender(false);
+            SetGametag("Untagged");
+
+            state = 2f;
         }
-        if (timer <= 3f && state == 1f)
+        if (timer <= 1f && state == 1f)
         {
+            SetRender(true);
+
             SetMaterial(Color.yellow);
             state = 2f;
         }
-        if (timer <= 1f && state == 2f)
+        if (timer <= 0.5f && state == 2f)
         {
+            SetRender(true);
+            SetGametag("dmgObject");
             SetMaterial(Color.red);
+            
             state = 3f;
             
+
         }
         if (timer <= 0f && state == 3f)
         {
             state = 0f;
-            timer = 10f;
+            timer = 3f;
         }
 
 
@@ -52,10 +67,10 @@ public class EnemyShocker : MonoBehaviour
 
     }
 
-    
 
-     
-    
+
+
+
     void SetMaterial(Color colors)
     {
         MaterialZone = GetComponentsInChildren<MeshRenderer>();
@@ -66,8 +81,60 @@ public class EnemyShocker : MonoBehaviour
             }
 
 
-        //MeshRenderer i = this.GetComponent<MeshRenderer>();
-        //i.material.color = Color.blue;
+
+    }
+
+    void SetRender(bool state)
+    {
+        rend = GetComponentsInChildren<Renderer>();
+        foreach (Renderer Zone in rend)
+            if (Zone.name == "ShockZone")
+            {
+                Zone.enabled = state;
+            }
+    }
+    void SetGametag(string tag)
+    {
+        tagg = GetComponentsInChildren<Transform>();
+        foreach (Transform Zone in tagg)
+            if (Zone.name == "ShockZone")
+            {
+                transform.gameObject.tag = tag;
+            }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Baton Pivot")
+        {
+            Destroy(gameObject);
+            Room.RemoveEnemy(this);
+        }
+    }
+    public override void SetTarget(PlayerController target)
+    {
+        Transform goal= this.target;
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.position);
+    }
+     //not working
+    IEnumerator ZoneScale()
+    {
+        scale = GetComponentsInChildren<Transform>();
+        foreach (Transform Zone in scale)
+            if (Zone.name == "ShockZone")
+            {
+               
+                while( size < 4f)
+                {
+                    
+                    //Zone.transform.localScale = new Vector3(size, 5f, size);
+                    transform.localScale = new Vector3(size, 5f, size);
+                    size += 1f;
+                    yield return new WaitForSeconds(1f);
+                }
+                       
+            }
+
 
     }
 }
